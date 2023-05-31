52deb164e53dc3ef62335a873e6d595a798dec02
DagmaraSobczak <daga.sobczak@gmail.com>
2023-05-31 21:59:20 +0200

    src

---
+++ src/components/Statistic/Statistic.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import css from './Statistic.module.css';
+
+export default class Statistic extends Component {
+  static propTypes = {
+    good: PropTypes.number,
+    neutral: PropTypes.number,
+    bad: PropTypes.number,
+  };
+
+  render() {
+    const { good, neutral, bad, total } = this.props;
+    return (
+      <>
+        <p>Statistics</p>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>Total: {total}</p>
+        <p> Positive feedback: {this.props.positivePercentage} %</p>
+      </>
+    );
+  }
+}
